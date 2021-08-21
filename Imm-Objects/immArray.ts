434d5a85ffe2895f1fe7cd80372ef938e7ec7b72
dzimentey <60585408+dzimentey@users.noreply.github.com>
2021-08-21 16:22:51 +0300

    Update immArray.ts

--- Imm-Objects/immArray.ts
+++ Imm-Objects/immArray.ts
@@ -1,1 +1,17 @@
+import {UserWithLapTopType} from "./instance";
 
+export type userWithBooks = {
+    books: string[] // ~ Array<sting>
+}
+
+export const addBook = (user: userWithBooks & UserWithLapTopType, book: string) => {
+    return {...user, books: [...user.books, book]}
+}
+
+export const addBooksArray = (user:userWithBooks & UserWithLapTopType, booksArray: Array<string>) => (
+    {...user, books: [...user.books, ...booksArray]}
+)
+
+export const changeBook = (user:userWithBooks & UserWithLapTopType, oldBook: string, newBook: string) => {
+    return { user, books: user.books.map(b => b === oldBook ? newBook : b) }
+}
