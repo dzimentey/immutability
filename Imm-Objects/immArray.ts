c5e0f89ea8844929dfdcfe927bab88f0a912ce86
dzimentey <60585408+dzimentey@users.noreply.github.com>
2021-08-24 20:41:54 +0300

    function updateCompanyTitle was added

--- Imm-Objects/immArray.ts
+++ Imm-Objects/immArray.ts
@@ -4,17 +4,17 @@ export type userWithBooks = {
     books: string[] // ~ Array<sting>
 }
 
+export type userCompaniesType = {
+    companies: Array<{ title: string, id: number }>
+}
+
 export const addBook = (user: userWithBooks & UserWithLapTopType, book: string) => {
     return {...user, books: [...user.books, book]}
 }
 
-export const addBooksArray = (user:userWithBooks & UserWithLapTopType, booksArray: Array<string>) => (
+export const addBooksArray = (user: userWithBooks & UserWithLapTopType, booksArray: Array<string>) => (
     {...user, books: [...user.books, ...booksArray]}
 )
-
-export const changeBook = (user:userWithBooks & UserWithLapTopType, oldBook: string, newBook: string) => {
-    return { user, books: user.books.map(b => b === oldBook ? newBook : b) }
-}
 
 export const changeBook = (user: userWithBooks & UserWithLapTopType, oldBook: string, newBook: string) => {
     return {user, books: user.books.map(b => b === oldBook ? newBook : b)}
@@ -27,3 +27,13 @@ export const removeBook = (user: userWithBooks & UserWithLapTopType, boringBook:
 export const addCompany = (user: UserWithLapTopType & userCompaniesType, company: { id: number, title: string }) => {
     return {...user, companies: [...user.companies, company]}
 }
+
+export const updateCompanyTitle = (user: UserWithLapTopType & userCompaniesType, id: number, newTitle: string) => {
+    return {...user, companies: user.companies.map(c => c.id === id ? {...c, title: newTitle} : c)}
+}
+
+// type companiesType = {
+//     id: number
+//     title: string
+// }
+
